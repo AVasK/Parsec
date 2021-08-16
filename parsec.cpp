c70691b0ae05e9c60f74b38577ee2886824ae442
AVasK <avask17@gmail.com>
2021-08-16 16:22:23 +0300

    slightly more complicated example

--- parsec.cpp
+++ parsec.cpp
@@ -397,10 +397,10 @@ int main() {
     // auto parser = make_parser("(" << value<int>() << "," << value<int>()
     //                         << "," << value<std::string>().then(")") );
 
-    auto parser = make_parser("(" << repeat<3>(value<int>() << ",")
-                              << value<std::string>().then(")") );
+    auto parser = make_parser("([" << repeat<3>(value<int>() << ",")
+                              << "] " << value<std::string>().then(")") );
 
 
-    auto [x,y] = parser.parse("(123,456,789,abc)");
-    std::cout << x[2] << ", "<< y;
+    auto [x,y] = parser.parse("([11,22,33,] a string)");
+    std::cout << x[2] << ", "<< y << "\n";
 }
